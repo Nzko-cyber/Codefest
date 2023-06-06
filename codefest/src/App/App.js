004c574d7bd4a7b80b4f29035bc145bf87f47d63
Quvonch <quvonchtoshtemirov@gmail.com>
2023-06-06 14:26:31 +0500

    footer

---
+++ codefest/src/App/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './App.css'
+import Footer from '../Components/Footer/Footer'
+import Banner from '../Components/Banner/banner'
+
+export default function App() {
+  return (
+    <div>
+      <Footer/>
+    </div>
+  )
+}
